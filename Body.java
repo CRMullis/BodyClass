c13fb90a388eaaed0cc87aada88b91bfd231aa4b
Collin Mullis <cmullis@saintjoe.edu>
2015-02-09 15:05:16 -0500

    Typed Code into Terminl

---
+++ Body.java
@@ -0,0 +1,10 @@
+Body sun = new Body();
+sun.idNum = Body.nextID++;
+sun.name = "Sol";
+sun.orbits = null; // in solar system, sun is middle
+
+Body earth = new Body();
+earth.idNum = Body.nextID++;
+earth.name = "Earth";
+earth.orbits = sun;
+
